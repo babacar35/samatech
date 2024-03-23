b7b898f4423b4207fcfe1bb1b53de359c059dbd3
Babacar Toure <toure.babacar35lv.as@gmail.com>
2024-03-23 14:54:55 +0000

    done for vercel config deploy corrigé db postgrsql  v4.1

--- Gestion_Vehicules/settings.py
+++ Gestion_Vehicules/settings.py
@@ -150,7 +150,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = 'static/'
 #static files
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
